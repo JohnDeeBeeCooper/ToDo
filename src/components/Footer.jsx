dd1efe9a5fdff5b72d53642b487c35c19f5121f3
JohnDeeBeeCooper <vladislav8519@gmail.com>
2018-07-20 10:35:46 +0300

    one more fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,30 @@
 import React from 'react'
 import cn from 'classnames';
 
-export default class extends React.Component {
-    render() {
-        const item = this.props;
-        const btnAllClass = cn({
-            btn: true,
-            active: item.display === "All" && true
-        });
-        const btnActiveClass = cn({
-            btn: true,
-            active: item.display === "Active" && true
-        });
-        const btnCompletedClass = cn({
-            btn: true,
-            active: item.display === "Completed" && true
-        });
-        return (
-            <div className="todo-footer">
-                <span className="todo-count">{item.count}  items left</span>
-                <ul className="footer-list">
-                    <li><button className={btnAllClass} onClick={item.all}>All</button></li>
-                    <li><button className={btnActiveClass} onClick={item.active}>Active</button></li>
-                    <li><button className={btnCompletedClass} onClick={item.completed}>Completed</button></li>
-                    <li><button onClick={this.props.delete} className="btn clear-completed">Clear completed</button></li>
-                </ul>
+const Footer = (props) => {
+    const btnAllClass = cn({
+        btn: true,
+        active: props.display === "All"
+    });
+    const btnActiveClass = cn({
+        btn: true,
+        active: props.display === "Active"
+    });
+    const btnCompletedClass = cn({
+        btn: true,
+        active: props.display === "Completed"
+    });
+    return (
+        <div className="todo-footer">
+            <span className="todo-count">{props.count}  items left</span>
+            <ul className="footer-list">
+                <li><button className={btnAllClass} onClick={props.all}>All</button></li>
+                <li><button className={btnActiveClass} onClick={props.active}>Active</button></li>
+                <li><button className={btnCompletedClass} onClick={props.completed}>Completed</button></li>
+                <li><button onClick={props.delete} className="btn clear-completed">Clear completed</button></li>
+            </ul>
 
-            </div>
-        );
-    }
+        </div>
+    );
 }
+export default Footer;
